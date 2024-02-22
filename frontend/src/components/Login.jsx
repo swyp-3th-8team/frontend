f46bee590cb9d75ff71eecb0a5542e3dd19740eb
Jisang Hong <jisang1024@gmail.com>
2024-02-22 22:54:23 +0900

    Connect page Login to Signup

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,9 @@
 import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.continer}>
       <div className={styles.loginIntro}>
         <h1>로그인</h1>
         <div>서비스 간단한 문구</div>
@@ -21,7 +22,9 @@ export default function Login() {
       </div>
       <div className={styles.button}>
         <button className={styles.loginButton}>로그인</button>
-        <button className={styles.signupButton}>회원가입</button>
+        <Link className={styles.signupButton} to="/signup">
+          회원가입
+        </Link>
       </div>
       <button className={styles.service}>서비스 미리보기</button>
     </div>
