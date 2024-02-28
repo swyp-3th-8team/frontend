053a10da8e9f5a0aef77df84e1013c688c9f892f
Jisang Hong <jisang1024@gmail.com>
2024-02-29 03:52:45 +0900

    Implement SubjectList

---
+++ frontend/src/components/Mandarlart/SubjectList.jsx
@@ -0,0 +1,24 @@
+import Button from "../Button";
+import styles from "./SubjectList.module.scss";
+
+export default function SubjectList() {
+  return (
+    <div className={styles.subjectList}>
+      <div className={styles.subject}>
+        <input type="text" placeholder="대주제를 등록해주세요" />
+        <button>✏️</button>
+      </div>
+      <div className={styles.mainGoal}>
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+        <input type="text" placeholder="목표를 입력해주세요" />
+      </div>
+      <Button finishButton="저장하기" />
+    </div>
+  );
+}
