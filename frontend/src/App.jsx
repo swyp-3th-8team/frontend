879e6ffd4739df30676f3872770939716aee38fb
Jisang Hong <jisang1024@gmail.com>
2024-02-29 14:51:08 +0900

    Implement HeaderLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
