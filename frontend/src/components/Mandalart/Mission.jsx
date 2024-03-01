45ed28ea58036fcc277c2322153c8fe89ca8b890
Jisang Hong <jisang1024@gmail.com>
2024-03-01 19:56:33 +0900

    <Mission />에 missionTitle props 전달 오류 수정

--- frontend/src/components/Mandalart/Mission.jsx
+++ frontend/src/components/Mandalart/Mission.jsx
@@ -4,15 +4,15 @@ export default function Mission({ objectiveList, missionTitle }) {
   return (
     <>
       <div className={styles.mission}>
-        <div>{objectiveList?.objectiveList?.[0]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[1]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[2]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[3]?.content}</div>
+        <div>{objectiveList?.[0]?.content}</div>
+        <div>{objectiveList?.[1]?.content}</div>
+        <div>{objectiveList?.[2]?.content}</div>
+        <div>{objectiveList?.[3]?.content}</div>
         <div className={styles.mainMission}>{missionTitle}</div>
-        <div>{objectiveList?.objectiveList?.[4]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[5]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[6]?.content}</div>
-        <div>{objectiveList?.objectiveList?.[7]?.content}</div>
+        <div>{objectiveList?.[4]?.content}</div>
+        <div>{objectiveList?.[5]?.content}</div>
+        <div>{objectiveList?.[6]?.content}</div>
+        <div>{objectiveList?.[7]?.content}</div>
       </div>
     </>
   );
