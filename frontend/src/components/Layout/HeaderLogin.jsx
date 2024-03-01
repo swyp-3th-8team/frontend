d68405cdf6c3111d64ce65c6953f5466315820b9
Nam Min Ki <nam960216@gmail.com>
2024-03-02 00:22:43 +0900

    style: header 스타일 변경

--- frontend/src/components/Layout/HeaderLogin.jsx
+++ frontend/src/components/Layout/HeaderLogin.jsx
@@ -1,12 +1,16 @@
-//import styles from "./HeaderLogin.module.scss";
-
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styles from "./HeaderLogin.module.scss";
 
 export default function HeaderLogin() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Link to="/login">로그인</Link>
-      <Link to="/signup">회원가입</Link>
+    <div className={styles.wrapper}>
+      <button onClick={() => navigate("/login")} className={styles.login}>
+        로그인
+      </button>
+      <button onClick={() => navigate("/signup")} className={styles.signup}>
+        회원가입
+      </button>
     </div>
   );
 }
