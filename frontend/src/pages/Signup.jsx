16ee5dff754af9d60687c955bffc7515fbf83b14
Nam Min Ki <nam960216@gmail.com>
2024-03-02 12:00:18 +0900

    style: 회원가입 페이지 스타일 변경

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import styles from "./Signup.module.scss";
 import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import styles from "./Signup.module.scss";
 
 export default function Signup() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [repassword, setRepassword] = useState("");
+  const [passwordcheck, setPasswordcheck] = useState("");
 
   const handleChangeUsername = (e) => {
     setUsername(e.target.value);
@@ -19,47 +18,56 @@ export default function Signup() {
   };
 
   const handleChangePasswordCheck = (e) => {
-    setRepassword(e.target.value);
+    setPasswordcheck(e.target.value);
   };
 
   const handleSubmit = () => {
-    axios.post("http://localhost:9090/member/join", null, { params: { username, password, repassword } });
+    axios.post("http://localhost:9090/member/join", null, {
+      params: { username, password, passwordcheck },
+    });
     navigate("/login");
-    };
+  };
 
   return (
-    <div>
+    <div className={styles.container}>
       <h2>회원가입</h2>
-      <div>
+      <div className={styles.makeId}>
+        <label htmlFor="id">아이디를 입력해주세요.</label>
         <input
+          name="id"
           value={username}
           type="text"
-          label="아이디를 입력해주세요"
           placeholder="6자 이상 입력해주세요."
           onChange={(e) => handleChangeUsername(e)}
         />
-        <div className={styles.overlap}>
+        <div className={styles.duplicate}>
           <div>중복확인 메세지</div>
           <button>중복 확인하기</button>
         </div>
       </div>
       <div className={styles.makePw}>
+        <label htmlFor="pw">비밀번호를 입력해주세요.</label>
         <input
+          name="pw"
           value={password}
           type="password"
-          label="비밀번호를 입력해주세요"
           placeholder="영문, 숫자, 특수문자로 8자 이상 입력해주세요."
           onChange={(e) => handleChangePassword(e)}
         />
-        <input
-          value={repassword}
-          type="password"
-          label="비밀번호를 다시 한 번 확인해주세요"
-          placeholder="비밀번호를 다시 입력해주세요."
-          onChange={(e) => handleChangePasswordCheck(e)}
-        />
+        <div>
+          <label htmlFor="pwcheck">비밀번호를 다시 입력해주세요.</label>
+          <input
+            name="pwcheck"
+            value={passwordcheck}
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+            onChange={(e) => handleChangePasswordCheck(e)}
+          />
+        </div>
       </div>
-      <button onClick={handleSubmit}>회원가입 완료</button>
+      <button className={styles.submit} onClick={handleSubmit}>
+        회원가입 완료
+      </button>
     </div>
   );
 }
