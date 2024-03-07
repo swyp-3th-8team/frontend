7d625f7dfda33bf2b2b950f69e34a290338c7cc2
Jisang Hong <jisang1024@gmail.com>
2024-03-07 23:48:52 +0900

    design cell break-word

--- src/components/Mandalart/Mission.jsx
+++ src/components/Mandalart/Mission.jsx
@@ -3,17 +3,29 @@ import styles from "./Mission.module.scss";
 export default function Mission({ missionList, missionTitle, onClick }) {
   return (
     <>
-      <div className={styles.mission} onClick={onClick}>
-        <div>{missionList?.[0]?.content}</div>
-        <div>{missionList?.[1]?.content}</div>
-        <div>{missionList?.[2]?.content}</div>
-        <div>{missionList?.[3]?.content}</div>
-        <div className={styles.mainMission}>{missionTitle}</div>
-        <div>{missionList?.[4]?.content}</div>
-        <div>{missionList?.[5]?.content}</div>
-        <div>{missionList?.[6]?.content}</div>
-        <div>{missionList?.[7]?.content}</div>
+      <div className={styles.cellContainer} onClick={onClick}>
+        <Cell>{missionList?.[0]?.content}</Cell>
+        <Cell>{missionList?.[1]?.content}</Cell>
+        <Cell>{missionList?.[2]?.content}</Cell>
+        <Cell>{missionList?.[3]?.content}</Cell>
+        <Cell className="mainCell">{missionTitle}</Cell>
+        <Cell>{missionList?.[4]?.content}</Cell>
+        <Cell>{missionList?.[5]?.content}</Cell>
+        <Cell>{missionList?.[6]?.content}</Cell>
+        <Cell>{missionList?.[7]?.content}</Cell>
       </div>
     </>
+  );
+}
+
+function Cell({ children, className }) {
+  return (
+    <div
+      className={
+        className === "mainCell" ? styles.mainCell : styles.cellWrapper
+      }
+    >
+      <div className={styles.cell}>{children}</div>
+    </div>
   );
 }
