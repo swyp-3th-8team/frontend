635fd149ff58874cea6fa4eb6be01486bc7393b8
Jisang Hong <jisang1024@gmail.com>
2024-03-08 19:31:05 +0900

    Implement PreMainPageModal

--- src/pages/PreMainPage.jsx
+++ src/pages/PreMainPage.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 import Goal from "../components/Mandalart/Goal";
 import Mission from "../components/Mandalart/Mission";
+import Modal from "../components/Modal/Modal";
+import PreMainPageModal from "../components/Modal/PreMainPageModal";
 import styles from "./PreMainPage.module.scss";
 
 export default function PreMainPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModals = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className={styles.mainWrapper}>
-      <div className={styles.content}>
-        <Goal />
-        <Goal />
-        <Goal />
-        <Goal />
-        <Mission />
-        <Goal />
-        <Goal />
-        <Goal />
-        <Goal />
+    <>
+      <div className={styles.mainWrapper} onClick={openModal}>
+        <div className={styles.content}>
+          <Goal />
+          <Goal />
+          <Goal />
+          <Goal />
+          <Mission />
+          <Goal />
+          <Goal />
+          <Goal />
+          <Goal />
+        </div>
       </div>
-    </div>
+      {isModalOpen && (
+        <Modal onClose={closeModals} className={styles.preMainPageModal}>
+          <PreMainPageModal />
+        </Modal>
+      )}
+    </>
   );
 }
