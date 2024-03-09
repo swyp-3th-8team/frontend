e506aa7dc61f396533ed311cdd9b289718ec25fb
Jisang Hong <jisang1024@gmail.com>
2024-03-09 20:38:01 +0900

    add const hasContent

--- src/components/Lists/MissionListItem.jsx
+++ src/components/Lists/MissionListItem.jsx
@@ -7,6 +7,7 @@ import styles from "./MissionListItem.module.scss";
 export default function MissionListItem({ goalList, content, onChange }) {
   const { isPanelEditing } = useContext(MainPageContext);
   const isCompleted = goalList.every((task) => task.isCompleted);
+  const hasContent = content && content.trim() !== "";
 
   return (
     <li className={styles.listbox}>
@@ -22,7 +23,11 @@ export default function MissionListItem({ goalList, content, onChange }) {
           {content || "목표를 입력해주세요"}
         </span>
       )}
-      {isCompleted ? <img src={GreenCheckIcon} /> : <img src={CheckIcon} />}
+      {isCompleted ? (
+        <img src={GreenCheckIcon} />
+      ) : hasContent ? (
+        <img src={CheckIcon} />
+      ) : null}
     </li>
   );
 }
