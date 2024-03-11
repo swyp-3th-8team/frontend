f9ffe0bad167f6dbe7fab0917ccf8bc8b59c39c6
Jisang Hong <jisang1024@gmail.com>
2024-03-11 21:21:18 +0900

    design Goal, Mission cell margin

--- src/components/Mandalart/Goal.jsx
+++ src/components/Mandalart/Goal.jsx
@@ -3,26 +3,22 @@ import styles from "./Goal.module.scss";
 export default function Goal({ goalList, missionList, onClick }) {
   return (
     <div className={styles.cellContainer} onClick={onClick}>
-      <Cell>{goalList?.[0]?.content}</Cell>
-      <Cell>{goalList?.[1]?.content}</Cell>
-      <Cell>{goalList?.[2]?.content}</Cell>
-      <Cell>{goalList?.[3]?.content}</Cell>
-      <Cell className="mainCell">{missionList}</Cell>
-      <Cell>{goalList?.[4]?.content}</Cell>
-      <Cell>{goalList?.[5]?.content}</Cell>
-      <Cell>{goalList?.[6]?.content}</Cell>
-      <Cell>{goalList?.[7]?.content}</Cell>
+      <Cell className={styles.cellWrapper0}>{goalList?.[0]?.content}</Cell>
+      <Cell className={styles.cellWrapper1}>{goalList?.[1]?.content}</Cell>
+      <Cell className={styles.cellWrapper2}>{goalList?.[2]?.content}</Cell>
+      <Cell className={styles.cellWrapper3}>{goalList?.[3]?.content}</Cell>
+      <Cell className={styles.mainCell}>{missionList}</Cell>
+      <Cell className={styles.cellWrapper4}>{goalList?.[4]?.content}</Cell>
+      <Cell className={styles.cellWrapper5}>{goalList?.[5]?.content}</Cell>
+      <Cell className={styles.cellWrapper6}>{goalList?.[6]?.content}</Cell>
+      <Cell className={styles.cellWrapper7}>{goalList?.[7]?.content}</Cell>
     </div>
   );
 }
 
-function Cell({ children, className }) {
+function Cell({ className, children }) {
   return (
-    <div
-      className={
-        className === "mainCell" ? styles.mainCell : styles.cellWrapper
-      }
-    >
+    <div className={`${className}`}>
       <div className={styles.cell}>{children}</div>
     </div>
   );
--- src/components/Mandalart/Mission.jsx
+++ src/components/Mandalart/Mission.jsx
@@ -4,15 +4,15 @@ export default function Mission({ missionList, missionTitle, onClick }) {
   return (
     <>
       <div className={styles.cellContainer} onClick={onClick}>
-        <Cell>{missionList?.[0]?.content}</Cell>
-        <Cell>{missionList?.[1]?.content}</Cell>
-        <Cell>{missionList?.[2]?.content}</Cell>
-        <Cell>{missionList?.[3]?.content}</Cell>
-        <Cell className="mainCell">{missionTitle}</Cell>
-        <Cell>{missionList?.[4]?.content}</Cell>
-        <Cell>{missionList?.[5]?.content}</Cell>
-        <Cell>{missionList?.[6]?.content}</Cell>
-        <Cell>{missionList?.[7]?.content}</Cell>
+        <Cell className={styles.cellWrapper0}>{missionList?.[0]?.content}</Cell>
+        <Cell className={styles.cellWrapper1}>{missionList?.[1]?.content}</Cell>
+        <Cell className={styles.cellWrapper2}>{missionList?.[2]?.content}</Cell>
+        <Cell className={styles.cellWrapper3}>{missionList?.[3]?.content}</Cell>
+        <Cell className={styles.mainCell}>{missionTitle}</Cell>
+        <Cell className={styles.cellWrapper4}>{missionList?.[4]?.content}</Cell>
+        <Cell className={styles.cellWrapper5}>{missionList?.[5]?.content}</Cell>
+        <Cell className={styles.cellWrapper6}>{missionList?.[6]?.content}</Cell>
+        <Cell className={styles.cellWrapper7}>{missionList?.[7]?.content}</Cell>
       </div>
     </>
   );
@@ -20,11 +20,7 @@ export default function Mission({ missionList, missionTitle, onClick }) {
 
 function Cell({ children, className }) {
   return (
-    <div
-      className={
-        className === "mainCell" ? styles.mainCell : styles.cellWrapper
-      }
-    >
+    <div className={`${className}`}>
       <div className={styles.cell}>{children}</div>
     </div>
   );
