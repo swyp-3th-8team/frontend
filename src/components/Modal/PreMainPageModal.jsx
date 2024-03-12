c950ea4b6e62accd1f69ecd4a102665437510263
Jisang Hong <jisang1024@gmail.com>
2024-03-12 23:15:26 +0900

    fix: PreMainPage

--- src/components/Modal/PreMainPageModal.jsx
+++ src/components/Modal/PreMainPageModal.jsx
@@ -4,6 +4,7 @@ import styles from "./PreMainPageModal.module.scss";
 
 export default function PreMainPageModal() {
   const navigate = useNavigate();
+
   return (
     <div className={styles.preMainPageModalAlert}>
       <div>로그인을 먼저 완료해주세요</div>
