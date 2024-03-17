ddea578d3642cc8e722b1f75357a4c7028c90efd
Nam Min Ki <nam960216@gmail.com>
2024-03-17 15:07:21 +0900

    fix: div -> form

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -11,7 +11,8 @@ export default function ChangePassword() {
   const [newPassword, setNewPassword] = useState("");
   const [newRepassword, setNewRePassword] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     axios
       .post(`${SERVER_URL}/api/member/pwUpdate`, {
         userId,
@@ -25,52 +26,60 @@ export default function ChangePassword() {
   return (
     <div className={styles.container}>
       <span>비밀번호 변경하기</span>
-      <div className={styles.id}>
-        <label htmlFor="id">아이디를 입력해주세요.</label>
-        <Input
-          id="id"
-          value={userId}
-          placeholder="아이디"
-          onChange={(e) => setUserId(e.target.value)}
-        />
-      </div>
-      <div className={styles.password}>
-        <label htmlFor="password">
-          현재 비밀번호(혹은 임시 비밀번호)를 입력해주세요.
-        </label>
-        <Input
-          id="password"
-          value={password}
-          placeholder="영문,숫자로 8자- 20자 내로 입력해주세요."
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className={styles.newpassword}>
-        <label htmlFor="newpassword">새로운 비밀번호를 입력해주세요.</label>
-        <Input
-          id="newpassword"
-          value={newPassword}
-          placeholder="영문,숫자로 8자-20자 내로 입력해주세요."
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
-      </div>
-      <div className={styles.newpasswordCheck}>
-        <label htmlFor="newpasswordCheck">
-          새로운 비밀번호를 다시 한번 확인해주세요.
-        </label>
-        <Input
-          id="newpasswordCheck"
-          value={newRepassword}
-          placeholder="비밀번호를 다시 입력해주세요."
-          onChange={(e) => setNewRePassword(e.target.value)}
-        />
-      </div>
-      <Button size="large" isActive="id &&" onClick={handleSubmit}>
-        변경하기
-      </Button>
-      <Button size="large" color="white">
-        취소하기
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.id}>
+          <label htmlFor="id">아이디를 입력해주세요.</label>
+          <Input
+            id="id"
+            value={userId}
+            placeholder="아이디"
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
+        <div className={styles.password}>
+          <label htmlFor="password">
+            현재 비밀번호(혹은 임시 비밀번호)를 입력해주세요.
+          </label>
+          <Input
+            id="password"
+            value={password}
+            type="password"
+            placeholder="영문,숫자로 8자- 20자 내로 입력해주세요."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles.newpassword}>
+          <label htmlFor="newpassword">새로운 비밀번호를 입력해주세요.</label>
+          <Input
+            id="newpassword"
+            value={newPassword}
+            type="password"
+            placeholder="영문,숫자로 8자-20자 내로 입력해주세요."
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles.newpasswordCheck}>
+          <label htmlFor="newpasswordCheck">
+            새로운 비밀번호를 다시 한번 확인해주세요.
+          </label>
+          <Input
+            id="newpasswordCheck"
+            value={newRepassword}
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+            onChange={(e) => setNewRePassword(e.target.value)}
+          />
+        </div>
+        <Button
+          size="large"
+          isActive={userId && password && newPassword && newRepassword}
+        >
+          변경하기
+        </Button>
+        <Button size="large" color="white">
+          취소하기
+        </Button>
+      </form>
     </div>
   );
 }
