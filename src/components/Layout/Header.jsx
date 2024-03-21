113ca17543b49a2b43c4c064f5e3d19ec3782a20
Jisang Hong <jisang1024@gmail.com>
2024-03-21 14:45:01 +0900

    fix: page flow

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import HeaderLogin from "./HeaderLogin";
 import styles from "./Header.module.scss";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function Header() {
   const navigate = useNavigate();
+  const { isLogin } = useAuth();
+
   return (
     <header className={styles.wrapper}>
-      <img src="img/logo.svg" alt="" onClick={() => navigate("/")} />
+      {isLogin ? (
+        <img src="img/logo.svg" alt="" onClick={() => navigate("/mando")} />
+      ) : (
+        <img src="img/logo.svg" alt="" onClick={() => navigate("/")} />
+      )}
       <HeaderLogin />
     </header>
   );
