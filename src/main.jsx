6c87e191206d75b24b5dee1abca6bf566c48b10e
Nam Min Ki <nam960216@gmail.com>
2024-03-22 19:12:46 +0900

    fix: strict mode 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
@@ -13,20 +12,18 @@ import ChangePassword from "./pages/ChangePassword.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="mando" element={<MainPage />} />
-          <Route path="" element={<PreMainPage />} />
-          <Route path="find-account" element={<FindAccount />} />
-          <Route path="find-password" element={<FindPassword />} />
-          <Route path="change-password" element={<ChangePassword />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="pre-signup" element={<PreSignup />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="mando" element={<MainPage />} />
+        <Route path="" element={<PreMainPage />} />
+        <Route path="find-account" element={<FindAccount />} />
+        <Route path="find-password" element={<FindPassword />} />
+        <Route path="change-password" element={<ChangePassword />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="pre-signup" element={<PreSignup />} />
+    </Routes>
+  </BrowserRouter>
 );
